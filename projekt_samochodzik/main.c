b13893c2feab4696cdf80c83940c916f2c9e22c5
nitrazek <99548252+nitrazek@users.noreply.github.com>
2022-12-20 00:26:36 +0100

    Podstawa projektu
    Podstawowe pliki i kod do programu

---
+++ projekt_samochodzik/main.c
@@ -0,0 +1,50 @@
+#include <msp430x14x.h>
+#include "lcd.h"
+#include "portyLcd.h"
+
+//---------------- zmienne globalne ----------------
+unsigned int i = 0;
+
+void InitClock(void)
+{
+  BCSCTL1 |= XTS;
+
+  do
+  {
+    IFG1 &= ~OFIFG;
+    for (i = 0xFF; i > 0; i--)
+      ;
+  } while ((IFG1 & OFIFG) == OFIFG);
+
+  BCSCTL1 |= DIVA_1;
+  BCSCTL2 |= SELM0 | SELM1;
+
+  TACTL = TASSEL_1 + MC_1 + ID_3;
+  CCTL0 = CCIE;
+  CCR0 = 50000;
+
+  _EINT();
+}
+
+//------------------ main program ------------------
+void main(void)
+{
+  WDTCTL = WDTPW + WDTHOLD;
+
+  InitPortsLcd();
+  InitLCD();
+  clearDisplay();
+  InitClock();
+
+  while (1)
+  {
+    // Tu zaczynamy program
+    SEND_CHAR("A");
+  }
+}
+
+#pragma vector = TIMERA0_VECTOR
+__interrupt void Timer_A(void)
+{
+  // Tu używamy przerwań
+}
